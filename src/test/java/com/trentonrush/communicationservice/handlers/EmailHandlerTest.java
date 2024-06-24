91a4d5a1c07bcd0d1474b3dec62927922650f100
Trent Rush <trush081@gmail.com>
2024-06-23 23:34:31 -0500

    Update Spring Boot, update Tests

--- src/test/java/com/trentonrush/communicationservice/handlers/EmailHandlerTest.java
+++ src/test/java/com/trentonrush/communicationservice/handlers/EmailHandlerTest.java
@@ -1,11 +1,16 @@
 package com.trentonrush.communicationservice.handlers;
 
+import com.trentonrush.communicationservice.configs.CommunicationProperties;
+import com.trentonrush.communicationservice.configs.TestConfig;
 import com.trentonrush.communicationservice.models.Communication;
 import com.trentonrush.communicationservice.models.Message;
 import com.trentonrush.communicationservice.models.MessageDetails;
 import com.trentonrush.communicationservice.models.enums.MessageType;
 import com.trentonrush.communicationservice.repositories.CommunicationRepository;
+import com.trentonrush.communicationservice.services.LanguageDetectionService;
 import com.trentonrush.communicationservice.services.SendGridService;
+import com.trentonrush.communicationservice.utils.CommunicationConstants;
+import com.trentonrush.communicationservice.utils.TestConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -20,17 +25,19 @@ import static org.mockito.Mockito.*;
 class EmailHandlerTest {
 
     private SendGridService sendGridService;
+    private CommunicationProperties communicationProperties;
+    private LanguageDetectionService languageDetectionService;
     private CommunicationRepository communicationRepository;
     private EmailHandler emailHandler;
 
 
     @BeforeEach
-    public void setup() throws NoSuchFieldException, IllegalAccessException {
+    public void setup() {
         sendGridService = mock(SendGridService.class);
         communicationRepository = mock(CommunicationRepository.class);
-        emailHandler = new EmailHandler(sendGridService, communicationRepository);
-        setFieldValue(emailHandler, "trentonrush", "trentonrush@example.com");
-        setFieldValue(emailHandler, "ukpray", "ukpray@example.com");
+        languageDetectionService = mock(LanguageDetectionService.class);
+        communicationProperties = new TestConfig().communicationProperties();
+        emailHandler = new EmailHandler(communicationProperties, sendGridService, languageDetectionService, communicationRepository);
     }
 
     @Test
@@ -40,22 +47,23 @@ class EmailHandlerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"trentonrush", "granitesolutions", "ukpray", "unknown"})
-    void send_trentonrush(String source) {
-        Communication communication = buildCommunication(source, MessageType.EMAIL);
+    void send_TransactionalEmailSources(String source) {
+        Communication communication = buildCommunication(source, MessageType.EMAIL, CommunicationConstants.TRANSACTIONAL);
         doNothing().when(sendGridService).sendEmail(any(Message.class));
         doReturn(communication).when(communicationRepository).save(any(Communication.class));
 
         emailHandler.send(communication);
-        assertEquals(Objects.equals(source, "ukpray") ? "ukpray@example.com" : "trentonrush@example.com", communication.getMessage().getSender());
+        assertEquals(Objects.equals(source, CommunicationConstants.UK_PRAY) ? TestConstants.UKPRAY_TEST_EMAIL : TestConstants.TRENTONRUSH_TEST_EMAIL, communication.getMessage().getSender());
         verify(communicationRepository, times(2)).save(communication);
         verify(sendGridService, times(1)).sendEmail(any(Message.class));
     }
 
-    public static Communication buildCommunication(String source, MessageType messageType) {
+    public static Communication buildCommunication(String source, MessageType messageType, String requestType) {
         Communication communication = new Communication();
         communication.setSource(source);
         communication.setMessageType(messageType);
         communication.setMessage(buildMessage());
+        communication.setRequestType(requestType);
         return communication;
     }
 
@@ -65,12 +73,5 @@ class EmailHandlerTest {
         message.setTemplate("test");
         message.setMessageDetails(new MessageDetails());
         return message;
-    }
-
-    // Helper method to set field value using reflection
-    private void setFieldValue(Object object, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
-        Field field = object.getClass().getDeclaredField(fieldName);
-        field.setAccessible(true);
-        field.set(object, value);
     }
 }
---
+++ src/test/java/com/trentonrush/communicationservice/utils/TestConstants.java
@@ -0,0 +1,7 @@
+package com.trentonrush.communicationservice.utils;
+
+public class TestConstants {
+
+    public static final String TRENTONRUSH_TEST_EMAIL = "trentonrush@test.com";
+    public static final String UKPRAY_TEST_EMAIL = "ukpray@test.com";
+}
